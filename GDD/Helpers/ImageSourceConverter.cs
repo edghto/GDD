6a9c049b846bd946b0436a87496e4f30cc3183bf
Mateusz Włodarczyk <wlodarczyk.mateusz@gmail.com>
2015-11-14 17:21:51 +0100

    Fixing file size displaying

--- GDD/Helpers/ImageSourceConverter.cs
+++ GDD/Helpers/ImageSourceConverter.cs
@@ -45,45 +45,4 @@ namespace GDD
             throw new NotImplementedException();
         }
     }
-    
-
-    public class SizeSourceConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            double size = (long)value;
-            string units = "bytes";
-            while(size > 1024 || units == "GB")
-            {
-                size = size / 1024;
-                units = bumpUnit(units);
-            }
-            return String.Format("{0:0.##} {1}", size, units);
-        }
-
-        private string bumpUnit(string units)
-        {
-            if(units == "bytes")
-            {
-                return "KB";
-            }
-            else if(units == "KB")
-            {
-                return "MB";
-            }
-            else if(units =="MB")
-            {
-                return "GB";
-            }
-            else
-            {
-                return "";
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ GDD/Helpers/SizeSourceConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace GDD
+{
+    public class SizeSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double size = (long)value;
+            string units = "bytes";
+
+            if (size == 0)
+                return "";
+
+            while (size > 1024 || units == "GB")
+            {
+                size = size / 1024;
+                units = bumpUnit(units);
+            }
+            return String.Format("{0:0.##} {1}", size, units);
+        }
+
+        private string bumpUnit(string units)
+        {
+            if (units == "bytes")
+            {
+                return "KB";
+            }
+            else if (units == "KB")
+            {
+                return "MB";
+            }
+            else if (units == "MB")
+            {
+                return "GB";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
